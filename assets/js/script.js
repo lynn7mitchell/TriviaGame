b33a661a1a1c8b238d049934695fc95a8b224159
Lynn Mitchell <lynn7mitchell@gmail.com>
2019-05-28 17:42:25 -0500

    added css reset

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var CountDown = 30;
+var qAndA = {
+    one: {
+        question: "Who said he did 3 amputations before his first breakfast",
+        correctAnswer: "Bj",
+        falseAnswers: ["Hawkeye", "Frank", "Colonel Potter"],
+    },
+    two: {
+        question: "Who told Hawkeye that Trapper had left for home",
+        answers: ["Radar", "Spearchucker", "Colonel Potter", "Frank"]
+    },
+    three: {
+        question: "How much money did the old korean villager who throws himself in front of vehicles want to be paid by the person who hit him?",
+        answer: ["$50", "$25", "$75", "$100"],
+    }
+}
+
+$("#start").on('click', function(){
+    
+})
