43ec713f27b95e38f437cf1346773681430261f7
lynn7mitchell <lynn7mitchell@gmail.com>
2019-05-28 20:06:23 -0500

    I can pick a random question and put it in an array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
 var CountDown = 30;
 var qAndA = {
-    one: {
+    zero: {
         question: "Who said he did 3 amputations before his first breakfast",
-        correctAnswer: "Bj",
-        falseAnswers: ["Hawkeye", "Frank", "Colonel Potter"],
+        answers: ["Bj", "Hawkeye", "Frank", "Colonel Potter"]
     },
-    two: {
+    one: {
         question: "Who told Hawkeye that Trapper had left for home",
         answers: ["Radar", "Spearchucker", "Colonel Potter", "Frank"]
     },
-    three: {
+    two: {
         question: "How much money did the old korean villager who throws himself in front of vehicles want to be paid by the person who hit him?",
         answer: ["$50", "$25", "$75", "$100"],
     }
+};
+var remainingQuestions = [];
+
+for(var i in qAndA){
+    remainingQuestions.push(qAndA[i]);
 }
 
-$("#start").on('click', function(){
-    
+$("#start-game").on('click', function(){
+    startQuestion();
 })
+function startQuestion(){
+    var randomQuestion = remainingQuestions[Math.floor(Math.random()*remainingQuestions.length)];
+    var currentQuestion = randomQuestion.question;
+    // console.log(currentQuestion)
+    remainingQuestions.splice(remainingQuestions.indexOf(randomQuestion))
+    // console.log(remainingQuestions)
+    randomNum = [Math.floor(Math.random() * 4 + 1)]
+    for(var i = 0; i <= 4; i++){
+        if($("div[attribue = ")
+    }
+
+    //get rid of currentQuestion in remainingQuestion
+}
